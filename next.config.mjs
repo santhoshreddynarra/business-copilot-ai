5fb03cff0309794ec4d428ff174c25013e66efed
Santhosh Reddy <santhoshreddynarra3029@gmail.com>
2026-06-22 19:39:24 +0530

    Initialize Next.js app and add configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
